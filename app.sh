e548c8fb08cb275cd264223a7a2be77a83a9e9b1
thuongpv <ckpvthuongft@gmail.com>
2022-05-27 01:21:01 +0700

    feat(dev): add shell script

--- app.sh
+++ app.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-if [ "$1"=="up" ]; then
+if [ "$1" == "up" ]; then
 	python manage.py runserver
+
+elif [ "$1" == "fmt" ]; then
+	autopep8 -r -i .
+	
+elif [ "$1" == "lint" ]; then
+	pre-commit run --all-files
 fi

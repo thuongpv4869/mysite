f54c914902d151fcbc7fad5ba6efd3c2fa9504f0
thuongpv <ckpvthuongft@gmail.com>
2022-05-26 20:10:07 +0700

    feat(dev): add  debug toolbar

--- mysite/urls.py
+++ mysite/urls.py
@@ -19,4 +19,6 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
+
+    path('debug/', include('debug_toolbar.urls')),
 ]

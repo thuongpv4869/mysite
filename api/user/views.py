87fdc9fa6970b4b91999cf840e3238409d56f73f
thuongpv <ckpvthuongft@gmail.com>
2022-06-01 20:09:13 +0700

    feat(api): api user_profile add schema

--- api/user/views.py
+++ api/user/views.py
@@ -1,17 +1,23 @@
 from rest_framework.views import APIView
 from api.common import get_api_response, get_model_serializer
+from drf_spectacular.utils import (
+    extend_schema
+)
 
 from core.models import UserFullInfo
 
 
 class UserProfileView(APIView):
 
+    serializer_cls = get_model_serializer(
+        UserFullInfo,
+        list_field=['id', 'email', 'first_name', 'last_name'])
+
+    @extend_schema(
+        responses={200: serializer_cls}
+    )
     def get(self, request, user_id):
         user_full = UserFullInfo.objects.get(pk=user_id)
 
-        serializer_cls = get_model_serializer(
-            UserFullInfo,
-            list_field=['id', 'email', 'first_name', 'last_name'])
-
-        body = serializer_cls(user_full).data
+        body = self.serializer_cls(user_full).data
         return get_api_response(data=body)
